752b1e0f6abd2bfec9a54a0f0f78ffceac42edf6
Do Quang Giap <167546581+giapdq04@users.noreply.github.com>
2024-09-27 16:57:01 +0700

    Add LottieView animation to HomeScreen

--- App.js
+++ App.js
@@ -4,11 +4,18 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import ChatScreen from './src/screens/ChatScreen';
 const Stack = createNativeStackNavigator();
+import LottieView from "lottie-react-native";
 
 function HomeScreen({ navigation }) {
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>Home Screen</Text>
+            <LottieView
+                source={require("./src/chat.json")}
+                style={{ width: "60%", height: "40%" }}
+                autoPlay
+                loop
+            />
             <Button title="Go to Chat" onPress={() => navigation.navigate('Chat')} />
         </View>
     );
